28096130cc109c972592ea7888c3a63077a2fc1e
MiguelGonza1100685 <85491546+MiguelGonza1100685@users.noreply.github.com>
2021-06-07 07:43:01 -0400

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter file = new StreamWriter("Reg1.txt", true);
+
+            string nm = "", fn = "";
+            int ag = 0, cd = 0;
+
+            Console.WriteLine("------Ingrese la cedula");
+            ag = int.Parse(Console.ReadLine());
+            Console.WriteLine("------Ingrese la nombre");
+            nm = Console.ReadLine();
+            Console.WriteLine("------Ingrese la apellido");
+            fn = Console.ReadLine();
+            Console.WriteLine("------Ingrese la edad");
+            cd = int.Parse(Console.ReadLine());
+            file.Write(ag);
+            file.Write(nm);
+            file.Write(fn);
+            file.Write(cd);
+            file.Close();
+
+
+        }
+    }
+}
